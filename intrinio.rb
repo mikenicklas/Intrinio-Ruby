42881e7f15aa23e1c2eb9bd7151f9b1f7115ea14
Mike Nicklas <mike.nicklas@gmail.com>
2015-07-18 14:07:52 +0000

    Added standardized fundamentals support.

--- intrinio.rb
+++ intrinio.rb
@@ -23,8 +23,18 @@ class Intrinio
     self.class.get("/prices?ticker=#{ticker.upcase}", options)
   end
   
-  def merge_auth(options)
-    # merge authenticatation parameters to each call
-    options.merge!(basic_auth: @auth)
+  def standardized_fundamentals(ticker = nil, options = {})
+    defaults = {  statement: "income_statement",
+                  type: "FY" }
+    options[:query] = defaults
+    
+    merge_auth(options)
+    self.class.get("/fundamentals/standardized?ticker=#{ticker.upcase}", options) if ticker
   end
+  
+  private
+    def merge_auth(options)
+      # merge authenticatation parameters to each call
+      options.merge!(basic_auth: @auth)
+    end
 end
